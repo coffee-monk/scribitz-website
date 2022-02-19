41cf43f5258b13421a588dff9f78a2171dcd72b1
coffee-monk <coffeemonk@hotmail.com>
2022-02-19 16:49:46 -0500

    trying out components

--- src/components/nav/footer.js
+++ src/components/nav/footer.js
@@ -2,48 +2,56 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <a
+          href="https://flowbite.com"
+          target="_blank"
+          className="flex items-center mb-4 sm:mb-0"
+        >
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Scribitz
+          </span>
+        </a>
+        <ul className="flex flex-wrap items-center mb-6 sm:mb-0">
+          <li>
+            <a
+              href="#"
+              className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+            >
+              Regulations
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-sm text-gray-500 hover:underline dark:text-gray-400"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
-        <a href="https://flowbite.com" class="hover:underline" target="_blank">
-          Flowbite™
+        <a
+          href="https://flowbite.com"
+          target="_blank"
+          className="hover:underline"
+        >
+          Scribitz™
         </a>
         . All Rights Reserved.
       </span>
-      <ul class="flex flex-wrap items-center mt-3 sm:mt-0">
-        <li>
-          <a
-            href="#"
-            class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
-          >
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
-          >
-            Licensing
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="text-sm text-gray-500 hover:underline dark:text-gray-400"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
     </footer>
   )
 }
