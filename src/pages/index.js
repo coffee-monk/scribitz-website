f1f1c1d45e7339dca66fc9c88038e87f5cc6e8b3
coffee-monk <coffeemonk@hotmail.com>
2022-03-06 19:55:46 -0500

    minor tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 
 import Layout from "../components/layout/Layout"
-import Showcase from "../components/body/Showcase"
+import Showcase from "../components/body/index/Showcase"
+import Welcome from "../components/body/index/Welcome"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Showcase />
+      <Welcome />
     </Layout>
   )
 }
