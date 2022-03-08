7985c66b6c11b4451f2695a88e01fa10b8119739
coffee-monk <coffeemonk@hotmail.com>
2022-03-08 16:52:49 -0500

    regulatory compliant basic styling

--- src/components/body/index/RegulatoryCompliant.js
+++ src/components/body/index/RegulatoryCompliant.js
@@ -3,16 +3,25 @@ import React from "react"
 const RegulatoryCompliant = () => {
   return (
     <section id="RegulatoryCompliant" className="p-4 bg-primary">
-      <div>
-        <h1>DON'T GET YOUR PROJECT REJECTED</h1>
-        <h2>MAKE SURE YOUR CAPTIONS ARE REGULATORY COMPLIANT</h2>
-        <p>
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-2xl font-bold text-secondary">
+          DON'T GET YOUR PROJECT REJECTED
+        </h1>
+        <h2 className="text-3xl font-bold text-white">
+          MAKE SURE YOUR CAPTIONS ARE REGULATORY COMPLIANT
+        </h2>
+        <p className="text-white mt-2">
           We know there's value in the content you create, that's why our team
           works hard to make your captions compliant with the regulatory
           standards and guidelines: WCAG 3.0, AODA, ADA, FCC, and others!
         </p>
-        <p>Do you need your captions to meet accessibility regulations?</p>
-        <button>CONTACT US </button>
+        <p className="text-white mt-2">
+          Do you need your captions to meet accessibility regulations?
+        </p>
+
+        <button className="rounded-lg text-white bg-tertiary hover:bg-red-700 focus:ring-2 focus:ring-red-300 text-lg font-bold rounded text-lg px-5 py-2.5 text-center mt-4">
+          CONTACT US
+        </button>
       </div>
     </section>
   )
